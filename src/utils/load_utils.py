fix: normalisation for ref data, modified loss func for ref data

src/utils/load_utils.py
@@ -124,9 +124,9 @@ def load_all_transcript_embeddings(transcript_embeddings_dir: str)->Dict[str, to
     Returns:
         Dict[str, torch.Tensor]: Dictionary where the keys are the transcript filenames and the values are the respective torch embeddings.
     """
-    print(os.path.abspath(transcript_embeddings_dir))
     if not os.path.exists(transcript_embeddings_dir):
         raise Exception(f"No transcript embeddings exist at {transcript_embeddings_dir}. Create the transcript embeddings using the desired transformer first. Refer utils/process_transcripts.py")
+    print("Loading transcripts from " + os.path.abspath(transcript_embeddings_dir))
     filenames = os.listdir(transcript_embeddings_dir)
     # print(transcript_embeddings_dir, len(filenames))
     transcript_embeddings_dict = {}
@@ -249,8 +249,10 @@ def load_reference_style_embeddings(config, type:str):
             expr_data = data['exp'].cpu().detach().numpy().squeeze()
             pose_data = data['pose'].cpu().detach().numpy().squeeze()
             pkl_data.append(np.concatenate([expr_data, pose_data], axis=0))
-    unstd_data = np.expand_dims(np.array(pkl_data), axis=0)
-    mean, stddev = mean_std_swap(unstd_data)
+    unstd_data = np.array(pkl_data)
+    # mean, stddev = mean_std_swap(unstd_data)
+    mean =  unstd_data.mean(axis=0)[None, :]
+    stddev = unstd_data.std(axis=0)[None, :] + EPSILON
     std_data = (unstd_data - mean) / stddev
     return std_data, mean, stddev
 
@@ -267,8 +269,6 @@ def format_reference_style_embeddings(embeddings:np.ndarray, seq_len=64, batch_s
     """
     batched_embeddings = []
     i = 0
-    assert embeddings.shape[0] == 1
-    embeddings = embeddings[0]
     n_emb = len(embeddings)
     temp_array = []
     while True:
@@ -359,7 +359,7 @@ def load_data(config, pipeline, tag, rng, vqconfigs=None, segment_tag='',
     N = gt_windows.shape[0]
     if train_ratio > 1 or train_ratio < 0:
         train_ratio = 0.7
-    print(f"The train_ration = {train_ratio}")
+    print(f"The train_ratio = {train_ratio}")
     train_N = int(N * train_ratio)
     idx = np.random.permutation(N)
     train_idx, val_idx = idx[:train_N], idx[train_N:]
@@ -467,7 +467,8 @@ def calc_stats(config, vqconfigs, tag, pipeline, train_X, train_Y, train_audio):
 def mean_std_swap(data):
     """ helper function to calc std and mean """
     B,T,F = data.shape
-    mean = data.mean(axis=1).mean(axis=0)[np.newaxis,np.newaxis,:]
+    # data (1, 169, 56)
+    mean = data.mean(axis=1).mean(axis=0)[np.newaxis,np.newaxis,:] # (1, 1, 56)
     std =  data.std(axis=1).std(axis=0)[np.newaxis,np.newaxis,:]
     std += EPSILON
     return mean, std

src/vqgan/train_vq_transformer.py
@@ -37,17 +37,21 @@ def generator_train_step(config, epoch, generator, g_optimizer, train_X,
     """
 
     if style_transfer:
-        train_X = np.repeat(train_X, repeats=2, axis=0) # repeating every element twice to train once with more and less style embeddings respectively
+        # train_X = np.repeat(train_X, repeats=2, axis=0) # repeating every element twice to train once with more and less style embeddings respectively
+        # [face1 face2  face3  face4  face5  face6  face7  face8]
+        orig_len = train_X.shape[0]
+        train_X = np.concatenate([train_X, train_X], axis=0) 
         gt_more = Variable(torch.from_numpy(more_style_embeddings_batch),
                           requires_grad=False).cuda()
         gt_less = Variable(torch.from_numpy(less_style_embeddings_batch),
                           requires_grad=False).cuda()
         beta_style_transfer = config['style_transfer']['loss']['beta_style_transfer']
-        token_more = torch.ones(1)
-        token_less = torch.zeros(1)
-
+        token_more = torch.ones(1, 1)
+        token_less = torch.zeros(1, 1)
+        print("line 49", len(train_X) == train_X.shape[0])
+        
     generator.train()
-    batchinds = np.arange(train_X.shape[0] // config['batch_size'])
+    batchinds = np.arange(len(train_X) // config['batch_size'])
     totalSteps = len(batchinds)
     rng.shuffle(batchinds)
     avgLoss = avgDLoss = 0
@@ -58,12 +62,22 @@ def generator_train_step(config, epoch, generator, g_optimizer, train_X,
         gtData = Variable(torch.from_numpy(gtData_np),
                         requires_grad=False).cuda()
         if style_transfer:
-            if bii % 2 == 0:
-                prediction, quant_loss = generator(gtData, None, style_token=token_less)
-                style_transfer_loss = calc_vq_loss(prediction, gt_less, quant_loss)
-            else:
+            if idxStart >= orig_len:
+                # use more expressive style embeddings
                 prediction, quant_loss = generator(gtData, None, style_token=token_more)
-                style_transfer_loss = calc_vq_loss(prediction, gt_more, quant_loss)
+                style_transfer_loss = calc_vq_loss(prediction, gt_more, quant_loss, quant_loss_weight=0)
+            else:
+                # use less expressive style embeddings
+                prediction, quant_loss = generator(gtData, None, style_token=token_less)
+                style_transfer_loss = calc_vq_loss(prediction, gt_less, quant_loss, quant_loss_weight=0)
+            # if bii % 2 == 0:
+            #     prediction, quant_loss = generator(gtData, None, style_token=token_less)
+            #     style_transfer_loss = calc_vq_loss(prediction, gt_less, quant_loss)
+            # else:
+            #     prediction, quant_loss = generator(gtData, None, style_token=token_more)
+            #     style_transfer_loss = calc_vq_loss(prediction, gt_more, quant_loss)
+            # prediction, quant_loss = generator(gtData, None, style_token=corresponding_style_tokens[idxStart:(idxStart + config['batch_size'])])
+            # style_transfer_loss = calc_vq_loss(prediction, corresponding_gt_styles[idxStart:(idxStart + config['batch_size'])], quant_loss)
         else:
             prediction, quant_loss = generator(gtData, None)
         g_loss = calc_vq_loss(prediction, gtData, quant_loss)
@@ -82,7 +96,7 @@ def generator_train_step(config, epoch, generator, g_optimizer, train_X,
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'\
                     .format(epoch, config['num_epochs'], bii, totalSteps,
                             avgLoss / totalSteps, np.exp(avgLoss / totalSteps)))
-            avg_Loss = 0
+            avgLoss = 0
 
     writer.add_scalar('Loss/train_totalLoss', avgLoss / totalSteps, epoch)
 
@@ -94,7 +108,9 @@ def generator_val_step(config, epoch, generator, g_optimizer, test_X,
     see generator_train_step() for parameter definitions
     """
     if style_transfer:
-        test_X = np.repeat(test_X, repeats=2, axis=0) # repeating every element twice to train once with more and less style embeddings respectively
+        orig_len = test_X.shape[0]
+        test_X = np.concatenate([test_X, test_X], axis=0) # repeating every element twice to train once with more and less style embeddings respectively
+        # test_X = np.repeat(test_X, repeats=2, axis=0) # repeating every element twice to train once with more and less style embeddings respectively
         gt_more = Variable(torch.from_numpy(more_style_embeddings_batch),
                           requires_grad=False).cuda()
         gt_less = Variable(torch.from_numpy(less_style_embeddings_batch),
@@ -117,12 +133,13 @@ def generator_val_step(config, epoch, generator, g_optimizer, test_X,
         with torch.no_grad():
             # prediction, quant_loss = generator(gtData, None)
             if style_transfer:
-                if bii % 2 == 0:
-                    prediction, quant_loss = generator(gtData, None, style_token=token_less)
-                    style_transfer_loss = calc_vq_loss(prediction, gt_less, quant_loss)
-                else:
+                # if bii % 2 == 0:
+                if idxStart >= orig_len:
                     prediction, quant_loss = generator(gtData, None, style_token=token_more)
                     style_transfer_loss = calc_vq_loss(prediction, gt_more, quant_loss)
+                else:
+                    prediction, quant_loss = generator(gtData, None, style_token=token_less)
+                    style_transfer_loss = calc_vq_loss(prediction, gt_less, quant_loss)
             else:
                 prediction, quant_loss = generator(gtData, None)
         g_loss = calc_vq_loss(prediction, gtData, quant_loss)
@@ -187,7 +204,10 @@ def main(args):
     ## setting up models
     fileName = config['model_path'] + \
                 '{}{}_best.pth'.format(tag, config['pipeline'])
-    load_path = fileName if os.path.exists(fileName) else None
+    if args.load_path is None:
+        load_path = fileName if os.path.exists(fileName) else None
+    else:
+        load_path = args.load_path
     generator, g_optimizer, start_epoch, style_transfer = setup_vq_transformer(args, config,
                                             version=None, load_path=load_path)
     generator.train()
@@ -201,10 +221,14 @@ def main(args):
     test_X = np.concatenate((test_listener[:,:seq_len,:],
                              test_listener[:,seq_len:,:]), axis=0)
     batch_size = config['batch_size']
+
+    # load reference style embeddings
     less_style_embeddings, less_mean, less_stddev = load_reference_style_embeddings(config, "less")
     more_style_embeddings, more_mean, more_stddev = load_reference_style_embeddings(config, "more")
     less_style_embeddings_batch = format_reference_style_embeddings(less_style_embeddings, seq_len=seq_len, batch_size=batch_size)
     more_style_embeddings_batch = format_reference_style_embeddings(more_style_embeddings, seq_len=seq_len, batch_size=batch_size)
+
+
     print('loaded listener...', train_X.shape, test_X.shape)
     print(f'Loaded reference style embeddings: {more_style_embeddings_batch.shape} {less_style_embeddings_batch.shape}')
     disc_factor = 0.0
@@ -229,5 +253,6 @@ if __name__ == '__main__':
     parser.add_argument('--checkpoint', type=str, default=None)
     parser.add_argument('--test', action='store_true')
     parser.add_argument('--ar_load', action='store_true')
+    parser.add_argument('--load_path', type=str, default=None)
     args = parser.parse_args()
     main(args)

src/vqgan/vqmodules/gan_models.py
@@ -40,32 +40,45 @@ def setup_vq_transformer(args, config, load_path=None, test=False, version=None)
                                   map_location=lambda storage, loc: storage)
         if style_transfer:
             generator.load_state_dict(loaded_state['state_dict'], strict=False) # made such changes so that able to load pretrained codebook weights without raising an error
-            generator.module.quantize.load_pretrained_codebook_weights(load_path, freeze_codebook=config['VQuantizer']['freeze_codebook'])
+            # generator.module.quantize.load_pretrained_codebook_weights(load_path, freeze_codebook=config['VQuantizer']['freeze_codebook'])
+            print(f"Loaded pretrained codebook weights from {load_path} for style transfer")
         else:
             generator.load_state_dict(loaded_state['state_dict'], strict=True) # made 
-        g_optimizer._optimizer.load_state_dict(
-                                loaded_state['optimizer']['optimizer'])
-        g_optimizer.set_n_steps(loaded_state['optimizer']['n_steps'])
-        start_epoch = loaded_state['epoch']
-        if start_epoch > 500:
+        # g_optimizer._optimizer.load_state_dict(
+        #                         loaded_state['optimizer']['optimizer'], strict=False)
+        # g_optimizer.set_n_steps(loaded_state['optimizer']['n_steps'])
+        # start_epoch = loaded_state['epoch']
+        if loaded_state['epoch'] > 500:
             print('>> changing lr to 4.5e-06')
             g_optimizer.set_init_lr(4.5e-06)
         print('loading checkpoint from...', load_path)
         del loaded_state
     else:
         print('starting from scratch...')
+
     if config['VQuantizer']['freeze_codebook']:
-        generator.module.quantize.freeze_codebook()
+        if style_transfer:
+            generator.module.freeze_all_except_style_transfer_layer()
+        else:
+            generator.module.quantize.embedding.weight.requires_grad = False
+            
+        for name, param in generator.module.named_parameters():
+            if param.requires_grad:
+                print(name, param.data)
+
     print(f"Froze codebook: {config['VQuantizer']['freeze_codebook']}")
+
     return generator, g_optimizer, start_epoch, style_transfer
 
 
 def calc_vq_loss(pred, target, quant_loss, quant_loss_weight=1.0, alpha=1.0):
     """ function that computes the various components of the VQ loss """
 
-    exp_loss = nn.L1Loss()(pred[:,:,:50], target[:,:,:50])
+    exp_loss = nn.L1Loss()(pred[:,:,:50], target[:,:,:50]) # expression loss
     rot_loss = nn.L1Loss()(pred[:,:,50:53], target[:,:,50:53])
     jaw_loss = alpha * nn.L1Loss()(pred[:,:,53:], target[:,:,53:])
+    # if quant_loss_weight == 0.0:
+    #     import pdb; pdb.set_trace()
     ## loss is VQ reconstruction + weighted pre-computed quantization loss
     return quant_loss.mean() * quant_loss_weight + \
             (exp_loss + rot_loss + jaw_loss)
@@ -96,6 +109,14 @@ class VQModelTransformer(nn.Module):
         else:
             quant, emb_loss, info = self.quantize(h) ## finds nearest quantization
         return quant, emb_loss, info
+
+    def freeze_all_except_style_transfer_layer(self):
+        self.quantize.freeze_codebook()
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+        for param in self.decoder.parameters():
+            param.requires_grad = False
+        print(f"Froze all except style transfer layer")
 
     def decode(self, quant):
         dec = self.decoder(quant) ## z' --> x

src/vqgan/vqmodules/quantizer.py
@@ -127,6 +127,8 @@ class StyleTransferVectorQuantizer(VectorQuantizer):
 
         # froze original codebook
         self.style_transfer_layer = nn.Linear(1, (n_e) * e_dim)
+        # torch.nn.init.constant_(self.style_transfer_layer.weight, 1)
+        # torch.nn.init.constant_(self.style_transfer_layer.bias, 0)
     
     def freeze_codebook(self):
         self.embedding.weight.requires_grad = False
@@ -146,7 +148,7 @@ class StyleTransferVectorQuantizer(VectorQuantizer):
 
     def forward(self, z, style_token):
         # Get style token embeddings in the shape of the codebook
-        style_token_emb = self.get_reshaped_style_token_embedding(style_token)
+        style_token_emb = self.get_style_token_embedding(style_token)
         # generate new embeddings layer from frozen codebook and the style token emb
         new_embedding_weights = self.embedding.weight * style_token_emb
         # generating quantized 
@@ -199,7 +201,7 @@ class StyleTransferVectorQuantizer(VectorQuantizer):
 
         return z_q, loss, (perplexity, min_encodings, min_encoding_indices)
 
-    def get_reshaped_style_token_embedding(self, style_token):
+    def get_style_token_embedding(self, style_token):
         # generate style token embedding
         style_token_emb = self.style_transfer_layer(style_token)
         # reshape the style token embedding to match the shape of the codebook
@@ -207,7 +209,7 @@ class StyleTransferVectorQuantizer(VectorQuantizer):
         return style_token_emb
 
     def get_distance(self, z, style_token):
-        style_token_emb = self.get_reshaped_style_token_embedding(style_token) # shape (n_e, e_dim), same as the codebook
+        style_token_emb = self.get_style_token_embedding(style_token) # shape (n_e, e_dim), same as the codebook
         z = z.permute(0, 2, 1).contiguous()
         z_flattened = z.view(-1, self.e_dim)
         # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
@@ -222,7 +224,7 @@ class StyleTransferVectorQuantizer(VectorQuantizer):
         return d
 
     def get_codebook_entry(self, indices, shape, style_token):
-        style_token_emb = self.get_reshaped_style_token_embedding(style_token) # shape (n_e, e_dim), same as the codebook
+        style_token_emb = self.get_style_token_embedding(style_token) # shape (n_e, e_dim), same as the codebook
         # shape specifying (batch, height, width, channel)
         # TODO: check for more easy handling with nn.Embedding
         min_encodings = torch.zeros(indices.shape[0], self.n_e).to(indices)